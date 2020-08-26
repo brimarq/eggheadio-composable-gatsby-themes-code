9dabf23fa44f8cc2de4afd40f08d9c8f018739a2
Brian Marquis <rbmarquis.dev@gmail.com>
2020-08-26 18:35:45 -0500

    use custom theme context on theme pages via emotion Global component

--- packages/gatsby-theme-marketing/src/pages/company.js
+++ packages/gatsby-theme-marketing/src/pages/company.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Global } from "@emotion/core";
+import { MyThemeContext } from "../context";
 import Header from "../components/header";
 import * as H from "../components/headings";
 import * as Text from "../components/text";
 
-export default props => (
-  <div>
-    <Header />
-    <H.h1>Company Page</H.h1>
-    <Text.p>about us and stuff</Text.p>
-    <H.h2>Employees</H.h2>
-    <Text.p>hopefully they don't leave</Text.p>
-    <H.h2>Investors</H.h2>
-    <Text.p>Thanks for money yo</Text.p>
-  </div>
-);
+export default props => {
+  const { theme } = useContext(MyThemeContext);
+  return (
+    <div>
+      {/** 
+       * Using the Global component here, in pages & templates controlled by the theme, is 
+       * ok because it will mount/unmount with the theme page/template. As a result, it will 
+       * only affect pages controlled by the theme (as opposed to those controlled by the user 
+       * or another theme).
+       */}
+      <Global styles={{ body: { backgroundColor: theme.colors.background } }} />
+      <Header />
+      <H.h1>Company Page</H.h1>
+      <Text.p>about us and stuff</Text.p>
+      <H.h2>Employees</H.h2>
+      <Text.p>hopefully they don't leave</Text.p>
+      <H.h2>Investors</H.h2>
+      <Text.p>Thanks for money yo</Text.p>
+    </div>
+  )
+};
--- packages/gatsby-theme-marketing/src/pages/pricing.js
+++ packages/gatsby-theme-marketing/src/pages/pricing.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Global } from "@emotion/core";
+import { MyThemeContext } from "../context";
 import Header from "../components/header";
 import * as H from '../components/headings'
 import * as Text from '../components/text'
 
-export default props => (
-  <div>
-    <Header />
-    <H.h1>SaaS App Pricing</H.h1>
-    <Text.p>like free tier, $30/mo "pro" tier, Enterprise tier</Text.p>
-  </div>
-);
+export default props => {
+  const { theme } = useContext(MyThemeContext);
+  return (
+    <div>
+      {/** 
+       * Using the Global component here, in pages & templates controlled by the theme, is 
+       * ok because it will mount/unmount with the theme page/template. As a result, it will 
+       * only affect pages controlled by the theme (as opposed to those controlled by the user 
+       * or another theme).
+       */}
+      <Global styles={{ body: { backgroundColor: theme.colors.background } }} />
+      <Header />
+      <H.h1>SaaS App Pricing</H.h1>
+      <Text.p>like free tier, $30/mo "pro" tier, Enterprise tier</Text.p>
+    </div>
+  );
+};
