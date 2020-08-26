a5ad14ae3d48b18ed212817412407d4e6dc79495
Brian Marquis <rbmarquis.dev@gmail.com>
2020-08-26 13:34:27 -0500

    remove global style declarations from theme

--- packages/gatsby-theme-marketing/wrap-root-element.js
+++ packages/gatsby-theme-marketing/wrap-root-element.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { ThemeProvider } from "theme-ui";
-import { Global } from "@emotion/core";
 import { deep } from "@theme-ui/presets";
 import * as H from './src/components/headings'
 import * as Text from './src/components/text'
@@ -12,7 +11,6 @@ const components = {
 
 export default ({ element }) => (
   <ThemeProvider theme={deep} components={components}>
-    <Global styles={{ body: { background: deep.colors.background } }} />
     {element}
   </ThemeProvider>
 );
