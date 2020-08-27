989b588129ffa504961fa41bb6aa66bb2901e67c
Brian Marquis <rbmarquis.dev@gmail.com>
2020-08-27 18:41:07 -0500

    create gatsby-config.js for theme, move gatsby-source-wordpress plugin into it from www

--- www/gatsby-config.js
+++ www/gatsby-config.js
@@ -11,20 +11,6 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     {
-      resolve: "gatsby-source-wordpress",
-      options: {
-        baseUrl: process.env.WORDPRESS_BASE_URL,
-        protocol: "https",
-        hostingWPCOM: true,
-        auth: {
-          wpcom_app_clientSecret: process.env.WORDPRESS_CLIENT_SECRET,
-          wpcom_app_clientId: process.env.WORDPRESS_CLIENT_ID,
-          wpcom_user: process.env.WORDPRESS_EMAIL,
-          wpcom_pass: process.env.WORDPRESS_PASSWORD
-        }
-      }
-    },
-    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: "dev-blog",
