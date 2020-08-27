24ae103edb9a69c0880eeab398549928dae2127f
Brian Marquis <rbmarquis.dev@gmail.com>
2020-08-27 14:11:46 -0500

    create gatsby-node.js to query wp pages (taken from www site)

---
+++ packages/gatsby-theme-product-blog/gatsby-node.js
@@ -0,0 +1,36 @@
+const path = require(`path`);
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions;
+  const wordPressPostTemplate = path.resolve(`src/templates/wordpress-blog-post.js`);
+  // Query for Mdx nodes to use in creating pages.
+  return graphql(`
+    query loadPagesQuery {
+      allWordpressPost {
+        nodes {
+          id
+          date
+          title
+          slug
+          content
+          excerpt
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      throw result.errors;
+    }
+
+    // Create blog post pages.
+    result.data.allWordpressPost.nodes.forEach( node => {
+      createPage({
+        path: `/blog/${node.slug}`,
+        component: wordPressPostTemplate,
+        context: {
+          id: node.id
+        }
+      })
+    })
+  });
+};
