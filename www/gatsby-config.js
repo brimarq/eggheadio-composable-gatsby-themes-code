b54f33dae3f97cb4d4abf4bbe9f6c77b693a682c
Brian Marquis <rbmarquis.dev@gmail.com>
2020-08-26 19:19:45 -0500

    add gatsby-theme-shopify to plugins

--- www/gatsby-config.js
+++ www/gatsby-config.js
@@ -5,6 +5,7 @@ require("dotenv").config({
 module.exports = {
   plugins: [
     `gatsby-theme-marketing`,
+    `gatsby-theme-shopify`,
     `gatsby-plugin-mdx`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
