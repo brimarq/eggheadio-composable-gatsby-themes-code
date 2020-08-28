a86a493c54011cc7839e58c526483823b3885825
Brian Marquis <rbmarquis.dev@gmail.com>
2020-08-28 17:22:50 -0500

    update page to use blogPost interface

--- packages/gatsby-theme-product-blog/src/pages/blog.js
+++ packages/gatsby-theme-product-blog/src/pages/blog.js
@@ -14,7 +14,7 @@ export default props => {
     <div>
       <Global styles={{ body: { backgroundColor: theme.colors.background } }} />
       <Header />
-      {props.data.allWordpressPost.nodes.map(node => (
+      {props.data.allBlogPost.nodes.map(node => (
         <div key={node.id}>
           <Text.Link to={`/blog/${node.slug}`}>
             <strong
@@ -41,7 +41,7 @@ export default props => {
 
 export const query = graphql`
   query AllProductBlogsPage {
-    allWordpressPost {
+    allBlogPost {
       nodes {
         id
         title
